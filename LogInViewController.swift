e4f6283dcc3c0dd146c4318f44710803afbedf76
arezhdo <arlinda.rezhdo@icloud.com>
2016-03-04 09:46:12 -0300

    Update LogInViewController.swift

--- LogInViewController.swift
+++ LogInViewController.swift
@@ -11,13 +11,68 @@ import Parse
 
 class LogInViewController: UIViewController {
     
-    //
     // Create Outlets for texfields
-    //
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    // Should not outorotate
+    
+    //
+    // MARK: - Actions
+    //
+    @IBAction func loginAction(sender: AnyObject) {
+        let username = self.usernameField.text
+        let password = self.passwordField.text
+        
+        // Validate the text fields: 
+        // If username is less than 5 characters display alert that shows the problem
+        if username?.characters.count < 5 {
+            let alert = UIAlertView(title: "Invalid", message: "Username must be greater than 5 characters", delegate: self, cancelButtonTitle: "OK")
+            alert.show()
+            
+        } 
+        // If password is less than 8 characters display alert that shows the problem
+        else if password?.characters.count < 8 {
+            let alert = UIAlertView(title: "Invalid", message: "Password must be greater than 8 characters", delegate: self, cancelButtonTitle: "OK")
+            alert.show()
+            
+        } 
+        // Otherwise continue with the login
+        else {
+            // Run a spinner to show a task in progress
+            let spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
+            spinner.startAnimating()
+            
+            // Send a request to login to Parse
+            PFUser.logInWithUsernameInBackground(username!, password: password!, block: { (user, error) -> Void in
+                
+                // Stop the spinner
+                spinner.stopAnimating()
+                
+                // If there was a username found in Parse, then show success 
+                if ((user) != nil) {
+                    let alert = UIAlertView(title: "Success", message: "Logged In", delegate: self, cancelButtonTitle: "OK")
+                    alert.show()
+                    
+                    // Instantiate View Controller with storyboard ID
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Tabbed")
+                        self.presentViewController(viewController, animated: true, completion: nil)
+                    })
+                    
+                } 
+                // Otherwise, show message of error
+                else {
+                    let alert = UIAlertView(title: "Error", message: "Your username or password do not match!", delegate: self, cancelButtonTitle: "OK")
+                    alert.show()
+                }
+            })
+        }
+    }
+    
+    //
+    // MARK: - Functions
+    //
+    // Screen should not outorotate for this particular View Controller
     override func shouldAutorotate() -> Bool {
         return false
     }
@@ -26,62 +81,18 @@ class LogInViewController: UIViewController {
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.Portrait
     }
-    
-    //
-    // Create Action for Log In Button
-    //
-    @IBAction func loginAction(sender: AnyObject) {
-        let username = self.usernameField.text
-        let password = self.passwordField.text
-        
-        // Validate the text fields
-        if username?.characters.count < 5 {
-            let alert = UIAlertView(title: "Invalid", message: "Username must be greater than 5 characters", delegate: self, cancelButtonTitle: "OK")
-            alert.show()
-            
-        } else if password?.characters.count < 8 {
-            let alert = UIAlertView(title: "Invalid", message: "Password must be greater than 8 characters", delegate: self, cancelButtonTitle: "OK")
-            alert.show()
-            
-        } else {
-            // Run a spinner to show a task in progress
-            let spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
-            spinner.startAnimating()
-            
-            // Send a request to login
-            PFUser.logInWithUsernameInBackground(username!, password: password!, block: { (user, error) -> Void in
-                
-                // Stop the spinner
-                spinner.stopAnimating()
-                
-                if ((user) != nil) {
-                    let alert = UIAlertView(title: "Success", message: "Logged In", delegate: self, cancelButtonTitle: "OK")
-                    alert.show()
-                    
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Tabbed")
-                        self.presentViewController(viewController, animated: true, completion: nil)
-                    })
-                    
-                } else {
-                    let alert = UIAlertView(title: "Error", message: "Your username or password do not match!", delegate: self, cancelButtonTitle: "OK")
-                    alert.show()
-                }
-            })
-        }
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    //
+    // MARK: - Navigation
+    //
+    // Connect buttons with exit view in .storyboard file
     @IBAction func unwindToLogInScreen(segue:UIStoryboardSegue) {
     }
     
